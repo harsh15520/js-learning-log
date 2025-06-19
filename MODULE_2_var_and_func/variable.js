ad1a7a4a7aa16fc60537a8f46baa495cb913fb39
harsh bansal <116622273+harsh15520@users.noreply.github.com>
2025-06-19 21:01:10 +0530

    Update variable.js
    examples for variables in JavaScript (let, const, var)

--- MODULE_2_var_and_func/variable.js
+++ MODULE_2_var_and_func/variable.js
@@ -1,4 +1,18 @@
-var a=24
-console.log(a)
-var greet="hello"
-console.log(greet)
+let nam='harry' ;//can change->used inside a function
+const number=23.2; //can not change
+var age=22 ;//can change->used when function call
+//multiple variables together declare
+let x=5, y=10,z=x+y;
+//storing variable inside variable, backtick(`) are used
+let message = `Hello, my name is ${nam}. I am ${age} years old.`;
+//output to html
+
+//runtime
+let isStudent = false;
+let score = null;
+let notDefined;
+//output 
+console.log("message:",message);
+console.log('isStudent',isStudent);
+console.log('score:',score);
+console.log('notDefined:',notDefined);
