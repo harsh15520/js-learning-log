6dd20072af974f1ca7068ce90c9bac6efa49fdd6
harsh bansal <116622273+harsh15520@users.noreply.github.com>
2025-06-21 19:03:54 +0530

    Create functions.js

---
+++ module_3/functions.js
@@ -0,0 +1,27 @@
+//truthy,falsy,equality(strict/loose)
+//The falsy values in JavaScript are specifically:
+
+//false,0 (and -0),"" (empty string),null,undefined,NaN (Not a Number)
+if("" || false || 0){
+    console.log('hi');
+}
+else{
+    console.log('bye');
+}
+var fluke='5';
+//loose equality
+if(fluke==5){
+    console.log("happy");
+}
+else{
+    console.log('angry');
+}
+//strict equality
+if(fluke===5){
+    console.log('bad');
+}
+else{
+    console.log('good');
+}
+//Only 0 (number zero) is falsy.
+//All other numbers, strings (except empty string ""), objects, arrays, functions, etc., are truthy.
